366f7531c81f55544ce4db238d3373329b633a38
JasperCDev <jasperischauvin@gmail.com>
2026-04-07 13:18:50 -0500

    componentize canvas

---
+++ app/org-visualizer/components/OrgVisCanvas.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { CanvasVisualizer } from "@/features/org-visualizer";
+
+export default function OrgVisCanvas() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const visualizer = new CanvasVisualizer(canvas);
+    visualizer.mount();
+
+    return () => visualizer.unmount();
+  }, []);
+
+  return <canvas ref={canvasRef} className="bg-black" />;
+}
--- app/org-visualizer/page.tsx
+++ app/org-visualizer/page.tsx
@@ -1,20 +1,6 @@
 "use client";
-
-import { useEffect, useRef } from "react";
-import { CanvasVisualizer } from "@/features/org-visualizer";
+import OrgVisCanvas from "./components/OrgVisCanvas";
 
 export default function OrgVisualizerPage() {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-
-    const visualizer = new CanvasVisualizer(canvas);
-    visualizer.mount();
-
-    return () => visualizer.unmount();
-  }, []);
-
-  return <canvas ref={canvasRef} className="bg-black" />;
+  return <OrgVisCanvas />;
 }
